680c381b414a411e35705d56aa960fedceefd0f1
Genaro Molina <genamolina01@gmail.com>
2024-03-19 20:30:15 -0300

    contador freecodecamp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from "./components/Button.jsx";
+import logoFreecodecamp from "./images/freecodecamp-logo.png";
 
 function App() {
+  const click = () => {
+    console.log("Click !");
+  };
+
+  const reiniciar = () => {
+    console.log("Reiniciar !");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="freecodecamp-logo-container">
+        <img
+          className="freecodecamp-logo"
+          src={logoFreecodecamp}
+          alt="logo de freecodecamp"
+        />
+      </div>
+      <div className="counter-container">
+        <Button
+          texto="Incrementar"
+          esBotonDeClick={true}
+          manejarClick={click}
+        />
+        <Button
+          texto="Reiniciar"
+          esBotonDeClick={false}
+          manejarClick={reiniciar}
+        />
+      </div>
     </div>
   );
 }
